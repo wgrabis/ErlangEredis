cfeedf36d002f69876b3770ae0ad1928f4ecd5cd
Piotr Heilman <piotr.heilman@gmail.com>
2015-01-20 10:04:38 +0100

    Added server behaviour. Fixed some issues.

--- src/users_stats.erl
+++ src/users_stats.erl
@@ -1,15 +1,73 @@
 -module(users_stats).
+-behaviour(gen_server).
 
--export([new/0, add/2, add_points/3, get_top_n/2]).
+-export([
+  start_link/0,
+  stop/0,
+  init/1,
+  handle_call/3,
+  handle_cast/2,
+  terminate/2
+]).
 
-new() ->
-  eredis:start_link().
+-export([
+  add/1,
+  add_points/2,
+  score/1,
+  top/1
+]).
 
-add(Conn, Username) ->
-  eredis:q(Conn, ["ZADD", "users", "0", Username]).
+start_link() ->
+  gen_server:start_link({local, users_stats}, users_stats, [], [])
+.
 
-add_points(Conn, Username, Points) ->
-  eredis:q(Conn, ["ZINCRBY", "users", Points, Username]).
+stop() ->
+  gen_server:cast(users_stats, stop)
+.
 
-get_top_n(Conn, N) ->
-  eredis:q(Conn, ["ZRANGE", "users", "0", N]).
+init([]) ->
+  {ok, Conn} = eredis:start_link(),
+  {ok, Conn}
+.
+
+terminate(Reason, _) ->
+  Reason,
+  ok
+.
+
+handle_call(Message, _From, Conn) ->
+  case Message of
+  {addUser, Name} ->
+    {ok, Reply} = eredis:q(Conn, ["ZADD", "users", "0", Name]),
+    {reply, Reply, Conn};
+  {addPoints, Name, Points} ->
+    {ok, Reply} = eredis:q(Conn, ["ZINCRBY", "users", Points, Name]),
+    {reply, Reply, Conn};
+  {score, Name} ->
+    {ok, Reply} = eredis:q(Conn, ["ZSCORE", "users", Name]),
+    {reply, Reply, Conn};
+  {topN, N} ->
+    {ok, Reply} = eredis:q(Conn, ["ZREVRANGE", "users", "0", N]),
+    {reply, Reply, Conn}
+  end
+.
+
+handle_cast(stop, Conn) ->
+  {stop, normal, Conn}
+.
+
+add(Name) ->
+  gen_server:call(users_stats, {addUser, Name})
+.
+
+add_points(Name, Points) ->
+  gen_server:call(users_stats, {addPoints, Name, Points})
+.
+
+score(Name) ->
+  gen_server:call(users_stats, {score, Name})
+.
+
+top(N) ->
+  gen_server:call(users_stats, {topN, N})
+.
