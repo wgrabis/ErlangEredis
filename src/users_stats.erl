c14eff89c6c965948c311d5c507faa8a01c734b6
Piotr Heilman <piotr.heilman@gmail.com>
2015-01-20 08:39:39 +0100

    Fixed user adding. Added script for run.

--- src/users_stats.erl
+++ src/users_stats.erl
@@ -1,6 +1,15 @@
 -module(users_stats).
 
--export([my_func/0]).
+-export([new/0, add/2, add_points/3, get_top_n/2]).
 
-my_func() ->
-    ok.
+new() ->
+  eredis:start_link().
+
+add(Conn, Username) ->
+  eredis:q(Conn, ["ZADD", "users", "0", Username]).
+
+add_points(Conn, Username, Points) ->
+  eredis:q(Conn, ["ZINCRBY", "users", Points, Username]).
+
+get_top_n(Conn, N) ->
+  eredis:q(Conn, ["ZRANGE", "users", "0", N]).
